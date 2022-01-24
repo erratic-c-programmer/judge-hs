1def5046063eabfe4e15968b8a22f64d620fb378
Isaac <tux314159@gmail.com>
2022-01-24 19:22:59 +0800

    heh, so apparently TimeSpec is a Num

--- src/backend/judgeBinary.hs
+++ src/backend/judgeBinary.hs
@@ -5,14 +5,14 @@ import System.Process
 
 type Testcase = (String, String)
 
-judgeBinary :: String -> Int -> [Testcase] -> IO [Bool]
+judgeBinary :: String -> Integer -> [Testcase] -> IO [Bool]
 judgeBinary progName timeLimit = mapM (judge progName)
   where
     judge progName t = do
-      TimeSpec t0_s t0_n <- getTime Monotonic
+      t0 <- getTime Monotonic
       (e, out, err) <- readProcessWithExitCode progName [] (fst t)
-      TimeSpec t1_s t1_n <- getTime Monotonic
+      t1 <- getTime Monotonic
       return $
         e == ExitSuccess
           && out == snd t
-          && ((t1_s - t0_s) * 1000 + (t1_n - t0_n) `div` 1000000) <= fromIntegral timeLimit
+          && toNanoSecs (t1 - t0) <= (timeLimit * 1000)
