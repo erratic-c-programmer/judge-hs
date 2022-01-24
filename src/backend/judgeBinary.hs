4d686d0fc21532c315c658cf5e6c8138273e4c06
Isaac <tux314159@gmail.com>
2022-01-24 21:56:11 +0800

    ooray

--- src/backend/judgeBinary.hs
+++ src/backend/judgeBinary.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import Data.List (sortBy)
+import Data.List.Split (splitOn)
+import Data.Maybe (catMaybes)
 import qualified Control.Monad.Parallel as PMon
 import System.Clock (Clock (Monotonic), getTime, toNanoSecs)
-import System.Exit (ExitCode (ExitSuccess))
+import System.Exit
+import System.IO
+import System.Directory
 import System.Process (readProcessWithExitCode)
+import Text.Read (readMaybe)
 
 type Testcase = (String, String)
 
-judgeBinary :: String -> Integer -> [Testcase] -> IO [Bool]
-judgeBinary progName timeLimit = PMon.mapM (judge progName)
+judgeBinaryTCs :: String -> Integer -> [Testcase] -> IO [Bool]
+judgeBinaryTCs progName timeLimit = PMon.mapM (judge progName)
   where
     judge progName t = do
       t0 <- getTime Monotonic
@@ -18,3 +24,21 @@ judgeBinary progName timeLimit = PMon.mapM (judge progName)
         e == ExitSuccess
           && out == snd t
           && toNanoSecs (t1 - t0) <= (timeLimit * 1000000)
+
+{-
+  testcase directory structure:
+  [problem]
+      1.1.in
+      1.2.out
+      2.1.in
+      2.2.out
+      ...
+      i.N.in
+      i.N.out
+    2
+    ...
+    N
+-}
+
+parseTCDir :: String -> IO [Testcase]
+parseTCDir dirName = do return []
